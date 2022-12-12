4c23d281416fbd9e77e90ace575eedcc1fdec60d
Nick Ronnei <nronnei@gmail.com>
2022-12-11 22:00:09 -0500

    Automates adding transcoding jobs. It all works great!

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+import 'dotenv/config.js';
 import conversion from './lib/media-convert/index.js';
 
 // Prove it's working
 (async () => {
   console.log('Publishing job');
+  console.log('DRY RUN?', process.env.CTO_DRY_RUN)
+  // Re-upload the current version
   const job = await conversion.publishDocumentaryJob();
+  // Publish a new version/template
+  // const job = await conversion.publishDocumentaryJob(true);
   console.log(job);
   process.exit(0);
 })();
